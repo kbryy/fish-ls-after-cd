ead0293483f359422d842658d4798d73d8ebe962
kbryy <kbry2124@gmail.com>
2025-01-06 16:19:58 +0900

    feat(plugin): allow custom command execution via LS_AFTER_CD_COMMAND

--- conf.d/ls-after-cd.fish
+++ conf.d/ls-after-cd.fish
@@ -3,9 +3,13 @@ if not set -q LS_AFTER_CD
     set -xg LS_AFTER_CD true
 end
 
+if not set -q LS_AFTER_CD_COMMAND
+    set -xg LS_AFTER_CD_COMMAND "ls -GF"
+end
+
 # ls automatically when changing directories
 function on_directory_change --on-variable PWD
     if test "$LS_AFTER_CD" = true; and status --is-interactive
-        ls -GF
+        eval $LS_AFTER_CD_COMMAND
     end
 end
