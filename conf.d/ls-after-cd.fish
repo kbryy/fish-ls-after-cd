bf91140b9d9f2b123a026e78a1024f0bf1156ca6
kbryy <kbry2124@gmail.com>
2025-01-08 21:27:07 +0900

    refactor: code and safety

--- conf.d/ls-after-cd.fish
+++ conf.d/ls-after-cd.fish
@@ -1,15 +1,16 @@
-#  Initialization of global variables
-if not set -q LS_AFTER_CD
-    set -xg LS_AFTER_CD true
+if not status --is-interactive
+    exit
 end
 
-if not set -q LS_AFTER_CD_COMMAND
-    set -xg LS_AFTER_CD_COMMAND "ls -GF"
-end
+#  Initialization of global variables
+set -q LS_AFTER_CD || set -gx LS_AFTER_CD true
+set -q LS_AFTER_CD_COMMAND || set -gx LS_AFTER_CD_COMMAND "ls -GF"
 
 # ls automatically when changing directories
 function on_directory_change --on-variable PWD
-    if test "$LS_AFTER_CD" = true; and status --is-interactive
+    if not string match -q "*;*" $LS_AFTER_CD_COMMAND
         eval $LS_AFTER_CD_COMMAND
+    else
+        echo "Error: LS_AFTER_CD_COMMAND contains invalid characters."
     end
 end
